f335623972a21a22abd47b86e6d767b5f0001a34
devIncogni <arinkumar10@outlook.com>
2025-06-15 11:05:15 +0530

    created a frequency counting hash function using hashmap

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
-#include "helloworld.h"
+#include <iostream>
+#include <unordered_map>
+
+void printFrequencies(int array[], int arraySize);
 
 int main() {
-  print("Hello World");
-  return 0;
+
+  // int arraySize{};
+
+  // std::cout << "Enter Array Size: ";
+  // std::cin >> arraySize;
+  // std::cout << '\n';
+
+  int array[20] = {1, 1,  1,  1,  2,  55, 55, 7, 6, 5,
+                   5, 10, 10, 10, 10, 9,  9,  8, 8, 8};
+  // for (int i{}; i < arraySize; ++i) {
+  //   std::cout << "Enter element at position " << i << ": ";
+  //   std::cin >> array[i];
+  //   // std::cout << '\n';
+  // }
+
+  printFrequencies(array, 20);
+}
+
+void printFrequencies(int array[], int arraySize) {
+  std::unordered_map<int, int> frequencies;
+
+  for (int i{}; i < arraySize; ++i) {
+    frequencies[array[i]] += 1;
+  }
+
+  for (auto it : frequencies) {
+    std::cout << it.first << ":\t" << it.second << '\n';
+  }
 }
